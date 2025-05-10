9b7316a47dcf2e3cfaf28195751954a3b5f1a816
Dinesh kharel <124329206+dineshkharel@users.noreply.github.com>
2025-05-10 21:05:14 +0545

    HeroSecction

--- src/Webpages/LandingPage.jsx
+++ src/Webpages/LandingPage.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import Navbar from "../Components/Navbar";
+import Herosection from "../Components/Herosection";
 
 const LandingPage = () => {
   return (
     <div>
       <Navbar />
+      <Herosection/>
       {/* We'll add Hero and Footer components here later */}
     </div>
   );
