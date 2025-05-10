6a878a163578a4a0d7c5e7bc907f3dee2028684d
prajjwal5 <prajjwalkrl5@gmail.com>
2025-05-10 14:17:52 +0545

    pk change

--- src/App.jsx
+++ src/App.jsx
@@ -9,26 +9,7 @@ const App = () =>{
         <h1 className="text-2xl font-bold">Hello, World!</h1>
         <p className="mt-2">This is a simple React app with Tailwind CSS.</p>
       </div>
-<<<<<<< HEAD
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-<<<<<<< HEAD
-        Click on the Vite and React logo to learn more Wauuuu
-=======
-     
->>>>>>> 0b146fa19f16c72777ec43e16c99fa213dbda48a
-      </p>
-=======
-      
->>>>>>> fa129cde9c5b767aa894c280204b7a288188b22d
+
     </>
   )
 }
