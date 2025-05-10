8f58fa2333ffed2d2a57ff444767b7d145f712e1
Dinesh kharel <124329206+dineshkharel@users.noreply.github.com>
2025-05-10 13:28:37 +0545

    Dinesh

--- src/App.jsx
+++ src/App.jsx
@@ -26,7 +26,7 @@ function App() {
         </p>
       </div>
       <p className="read-the-docs">
-        Click on the Vite and React logo to learn more Dinesh
+        Click on the Vite and React logo to learn more about Dinesh Kharel
       </p>
     </>
   )
