781d7e9ccd736b56a23c2a1a4477b0225e71b317
prajjwal5 <prajjwalkrl5@gmail.com>
2025-05-11 10:54:04 +0545

    Footer added

--- src/Webpages/LandingPage.jsx
+++ src/Webpages/LandingPage.jsx
@@ -2,13 +2,14 @@ import React from "react";
 import Navbar from "../Components/Navbar";
 import Herosection from "../Components/Herosection";
 import Features from "../Components/Features";
-
+import Footersection from "../Components/Footersection";
 const LandingPage = () => {
   return (
     <div>
       <Navbar />
       <Herosection/>
       <Features/>
+      <Footersection/>
       
     </div>
   );
