9380ba87ef1afe788063b56b75cbc1c5a59f099d
bikuvai205 <itsmeking205@gmail.com>
2025-05-12 11:23:50 +0545

    error on ui side space

--- src/Webpages/LandingPage.jsx
+++ src/Webpages/LandingPage.jsx
@@ -3,12 +3,15 @@ import Navbar from "../Components/Navbar";
 import Herosection from "../Components/Herosection";
 import Features from "../Components/Features";
 import Footersection from "../Components/Footersection";
+import About from "../Components/About";
 const LandingPage = () => {
   return (
     <div>
       <Navbar />
       <Herosection/>
       <Features/>
+      
+      
       <Footersection/>
       
     </div>
