a8114b0d5e3570f02e83944ff9c3b6897b0cba4d
Dinesh kharel <124329206+dineshkharel@users.noreply.github.com>
2025-05-12 20:54:49 +0545

    Getstartpage

---
+++ src/Webpages/GettingStartPage.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import GetNav from "../Components/Getting_Started/GettingStartNav";
+const GettingStartPage = () => {
+    return(
+    <GetNav/>
+    );
+};
+export default GettingStartPage;
