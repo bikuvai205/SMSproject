7c615ca0650608c397531bb73f629db449b457da
Dinesh kharel <124329206+dineshkharel@users.noreply.github.com>
2025-05-12 21:48:12 +0545

    getstart

--- src/Webpages/GettingStartPage.jsx
+++ src/Webpages/GettingStartPage.jsx
@@ -1,8 +1,12 @@
 import React from "react";
 import GetNav from "../Components/Getting_Started/GettingStartNav";
+import GetForm from "../Components/Getting_Started/GettingStartForm";
 const GettingStartPage = () => {
     return(
+    <>
     <GetNav/>
+    <GetForm/>
+    </>
     );
 };
 export default GettingStartPage;
